cdeef06692bb5452263b1c769df62def246e0322
Ethan Niser <ethan@niser.com>
2024-12-03 11:29:53 -0500

    formatting

--- solutions/1/main.hs
+++ solutions/1/main.hs
@@ -2,36 +2,36 @@ import Data.List (sort)
 
 part1 :: IO ()
 part1 = do
-  input <- readFile "input.txt"
-  print $ solve input
+    input <- readFile "input.txt"
+    print $ solve input
 
 solve :: String -> Int
 solve input =
-  let (column1, column2) = parseColumns input
-      sortedCol1 = sort column1
-      sortedCol2 = sort column2
-      diffs = zipWith (-) sortedCol1 sortedCol2
-      positiveDiffs = map abs diffs
-      result = sum positiveDiffs
-   in result
+    let (column1, column2) = parseColumns input
+        sortedCol1 = sort column1
+        sortedCol2 = sort column2
+        diffs = zipWith (-) sortedCol1 sortedCol2
+        positiveDiffs = map abs diffs
+        result = sum positiveDiffs
+     in result
 
 parseLine :: String -> [Int]
 parseLine = map read . words
 
 parseColumns :: String -> ([Int], [Int])
 parseColumns input =
-  let rows = map parseLine (lines input)
-      column1 = map head rows
-      column2 = map last rows
-   in (column1, column2)
+    let rows = map parseLine (lines input)
+        column1 = map head rows
+        column2 = map last rows
+     in (column1, column2)
 
 part2 :: IO ()
 part2 = do
-  input <- readFile "input.txt"
-  let (column1, column2) = parseColumns input
-      freqs = map (\x -> x * countItem x column1) column2
-      result = sum freqs
-  print result
+    input <- readFile "input.txt"
+    let (column1, column2) = parseColumns input
+        freqs = map (\x -> x * countItem x column1) column2
+        result = sum freqs
+    print result
 
 countItem :: (Eq a) => a -> [a] -> Int
 countItem x xs = length (filter (== x) xs)
