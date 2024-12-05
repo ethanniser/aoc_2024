42ec7cbc7f9dd57bf525b4d81088f3764d75ab79
Ethan Niser <ethan@niser.com>
2024-12-04 21:28:08 -0500

    day 4 done

--- solutions/4/Main.hs
+++ solutions/4/Main.hs
@@ -36,7 +36,29 @@ part1 = do
     let result = sum $ map searchForXMAS $ concat $ getPermutations input
     print result
 
+validCorners :: (Char, Char, Char, Char) -> Bool
+validCorners (topLeft, topRight, bottomLeft, bottomRight) = or [one, two, three, four]
+  where
+    one = and [topLeft == 'M', topRight == 'M', bottomLeft == 'S', bottomRight == 'S']
+    two = and [topLeft == 'M', topRight == 'S', bottomLeft == 'M', bottomRight == 'S']
+    three = and [topLeft == 'S', topRight == 'M', bottomLeft == 'S', bottomRight == 'M']
+    four = and [topLeft == 'S', topRight == 'S', bottomLeft == 'M', bottomRight == 'M']
+
+isValidX :: [[Char]] -> Bool
+isValidX x
+    | x !! 1 !! 1 == 'A' = validCorners (head (head x), head x !! 2, head (x !! 2), x !! 2 !! 2)
+    | otherwise = False
+
+get3by3Blocks :: [[Char]] -> [[[Char]]]
+get3by3Blocks grid =
+    [ [[grid !! (i + x) !! (j + y) | y <- [0 .. 2]] | x <- [0 .. 2]]
+    | i <- [0 .. length grid - 3]
+    , j <- [0 .. length (head grid) - 3]
+    ]
+
 part2 :: IO ()
 part2 = do
-    input <- readFile "test.txt"
-    print "hi"
+    input <- readFile "input.txt"
+    let lines' = lines input
+    let result = sum $ map (fromEnum . isValidX) $ get3by3Blocks lines'
+    print result
